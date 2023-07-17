dacad18be98800de5193b50e546dcc0816e9aa16
Arpan Kumar <108618629+arpan21020@users.noreply.github.com>
2023-07-18 02:20:38 +0530

    Update Footer.js

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -8,10 +8,10 @@ const Footer=()=>{
 
 
             <div className="icons">
-                <a href="https://www.facebook.com/cybertron.iiitd/" target="_blank"><i className="fa-brands fa-telegram"></i></a>
-                <a href="https://www.instagram.com/cybertron.iiitd/" target="_blank"><i className="fab fa-instagram" color="#fff"></i></a>
-                <a href="https://www.linkedin.com/company/cybertron-iiitd/" target="_blank"><i className="fab fa-linkedin-in" color="#fff"></i></a>
-                <a href="https://twitter.com/cybertron_iiitd" target="_blank"><i className="fab fa-twitter" color="#fff"></i></a>
+                <a href="https://www.facebook.com/cybertron.iiitd/" target="_blank"><i className="fa-brands fa-telegram" id="telegram"></i></a>
+                <a href="https://www.instagram.com/cyfuse_iiitd/" target="_blank"><i className="fab fa-instagram" id="insta" color="#fff"></i></a>
+                <a href="https://www.linkedin.com/company/cybertron-iiitd/" target="_blank"><i className="fab fa-linkedin-in" id="linkedin" color="#fff"></i></a>
+                <a href="https://twitter.com/cybertron_iiitd" target="_blank"><i className="fab fa-twitter" id="twitter" color="#fff"></i></a>
             </div>
             <div className="copyrights">
              <p>© 2021 CyFuse. All rights reserved.</p>
