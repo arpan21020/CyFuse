77b759a8f2a9e26879c1acf547c0ae269d16f003
arpan21020 <arpan21020@iiitd.ac.in>
2023-07-30 00:49:07 +0530

    linkedin added

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -10,7 +10,7 @@ const Footer=()=>{
             <div className="icons">
                 <a href="https://www.facebook.com/cybertron.iiitd/" target="_blank"><i className="fa-brands fa-telegram" id="telegram"></i></a>
                 <a href="https://www.instagram.com/cyfuse_iiitd/" target="_blank"><i className="fab fa-instagram" id="insta" color="#fff"></i></a>
-                <a href="https://www.linkedin.com/company/cybertron-iiitd/" target="_blank"><i className="fab fa-linkedin-in" id="linkedin" color="#fff"></i></a>
+                <a href="https://www.linkedin.com/company/cyfuse/" target="_blank"><i className="fab fa-linkedin-in" id="linkedin" color="#fff"></i></a>
                 <a href="https://twitter.com/cybertron_iiitd" target="_blank"><i className="fab fa-twitter" id="twitter" color="#fff"></i></a>
             </div>
             <div className="copyrights">
